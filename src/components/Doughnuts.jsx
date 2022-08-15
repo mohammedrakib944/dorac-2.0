1cdc47952ebbe6d696e693cc8530aba7138e3351
mohammedrakib944 <mohammedrakib944@gmail.com>
2022-08-15 15:06:21 +0600

    Nothing

--- src/components/Doughnuts.jsx
+++ src/components/Doughnuts.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Doughnut } from "react-chartjs-2";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Chart as ChartJS, ArcElement, Tooltip } from "chart.js";
 
 const Data = {
   labels: [
@@ -44,7 +44,7 @@ const Data = {
   ],
 };
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip);
 
 const Doughnuts = () => {
   return (
--- src/components/MembersCard.jsx
+++ src/components/MembersCard.jsx
@@ -1,10 +1,9 @@
 import React, { Children } from "react";
-import Man from "../assets/man.jpg";
-import { AiOutlineTwitter } from "react-icons/ai";
+import { AiOutlineTwitter, AiFillCloud } from "react-icons/ai";
 import { GrLinkedinOption } from "react-icons/gr";
 
 const MembersCard = ({ data }) => {
-  const { dp, name, title, linkdin, twitter } = data;
+  const { dp, name, title } = data;
   return (
     <div className="rounded-lg rounded-br-[30px] overflow-hidden min-w-[300px] mx-4 bg-violet-500/20 hover:bg-violet-800 duration-300">
       <img src={dp} className="w-[200px] rounded-lg mx-auto mt-4" alt="" />
@@ -12,12 +11,29 @@ const MembersCard = ({ data }) => {
         <h3 className="mt-4 text-teal-500">{name}</h3>
         <p className="py-2">{title}</p>
         <div className="w-full text-[24px] flex gap-4 items-center justify-center pb-6">
-          <a href={linkdin}>
-            <AiOutlineTwitter />
-          </a>
-          <a href={twitter}>
-            <GrLinkedinOption />
-          </a>
+          {data?.twitter && (
+            <a href={data.twitter} target="_blank">
+              <AiOutlineTwitter />
+            </a>
+          )}
+
+          {data?.linkdin && (
+            <a href={data.linkdin} target="_blank">
+              <GrLinkedinOption />
+            </a>
+          )}
+
+          {data?.cloud && (
+            <a href={data.cloud} target="_blank">
+              <AiFillCloud />
+            </a>
+          )}
+
+          {data?.web && (
+            <a href={data.web} target="_blank">
+              <AiFillCloud />
+            </a>
+          )}
         </div>
       </div>
     </div>
